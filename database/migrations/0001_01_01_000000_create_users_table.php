72cef6f40b892173ec4d840f05a2e4b88793e1fd
ilyas mohetna <ilyas.mohetna@hotmail.com>
2024-11-18 01:21:43 +0100

    add boolean to active or inactive user

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -19,6 +19,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+            $table->boolean('active')->default(1);
             $table->timestamps();
             $table->softDeletes();
         });
